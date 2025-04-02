37147f7a688485fdc099e206421ab4f1fb4cf1a2
Arseniy <salychsena@gmail.com>
2025-04-02 16:18:55 +0300

    1.1.0

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export',
+  basePath: '/history_dates',
+  styledComponents: true,
 };
 
 export default nextConfig;
